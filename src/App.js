d08adfefd921587083d1b13f3ecabd3fc63c03a7
Micheal Leverton <levertonmicheal@gmail.com>
2022-05-02 14:11:14 -0400

    Fun updating things

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        Decentraliz Lottery<br/><br/>Comming Soon Soon!
+        Decentraliz Lottery<br/><br/>Comming Soon Soon!<br/><br/>This is fun to watch update live!
       </header>
     </div>
   );
