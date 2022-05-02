9e77b2bc8f369e890d2abc7b2e09bc32a5be9eca
Micheal Leverton <levertonmicheal@gmail.com>
2022-05-02 14:48:40 -0400

    Text changes

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        Decentraliz Lottery<br/><br/>Comming Soon Soon!<br/><br/>This is fun to watch live updates!
+        <h1>Decentraliz Lottery</h1><h2>Coming Soon!</h2>This is fun to watch live update!
       </header>
     </div>
   );
