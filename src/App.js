b939803699d9989808522abc1501469117556296
Micheal Leverton <levertonmicheal@gmail.com>
2022-05-02 20:37:57 -0400

    Logo time

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="App-header bkimg">
         <h1>Decentraliz Lottery</h1><h2>Coming Soon!</h2>Home to the Shiba Inu Burn Lottery
       </header>
     </div>
