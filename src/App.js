3754e1468f7fe6fac757057ed26366d92bda237e
Micheal Leverton <levertonmicheal@gmail.com>
2022-05-02 22:14:37 -0400

    Ya this is the final one

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header bkimg">
-        <h1>Decentraliz Lottery</h1><h2>Coming Soon!</h2>Home to the Shiba Inu Burn Lottery
+        <h1>Decentraliz Lottery</h1><h2>Coming Soon!</h2><h3>Home to the Shiba Inu Burn Lottery</h3>
       </header>
     </div>
   );
